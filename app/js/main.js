635ab3c398d6e13f7a224991f8c4191f03e71fbf
kschmidt2 <k.schmidt2@gmail.com>
2019-10-30 17:28:02 -0400

    adds chart

--- app/js/main.js
+++ app/js/main.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             Highcharts.chart(chartId, {
                 chart: {
-                    type: 'bar',
+                    type: 'arearange',
                     styledMode: true,
                     spacingBottom: 25,
                     spacingRight: 100
@@ -53,37 +53,26 @@ document.addEventListener('DOMContentLoaded', function () {
                     text: null
                 },
                 data: {
-                    googleSpreadsheetKey: '1YOKb5l2VM4aAB2r20N_1aT_1vEajYrP3U-U3A6lZbC0'
+                    googleSpreadsheetKey: '19gPNciBoaDoDN3R5Ws2SQxBfrVL2jGk486llItuIvCE'
                 },
-                // for bar charts only
                 plotOptions: {
                     series: {
-                        groupPadding: 0.1
-                    } 
+                        lineWidth: 1,
+                        // clip: false,
+                        marker: {
+                            enabled: false,
+                            symbol: 'circle',
+                            fillColor: '#ffffff',
+                            states: {
+                                hover: {
+                                    fillColor: '#ffffff'
+                                }
+                            }
+                        }
+                    }
                 },
-                // for line charts only
-                // plotOptions: {
-                //     series: {
-                //         lineWidth: 1,
-                //         // clip: false,
-                //         marker: {
-                //             enabled: false,
-                //             symbol: 'circle',
-                //             fillColor: '#ffffff',
-                //             states: {
-                //                 hover: {
-                //                     fillColor: '#ffffff'
-                //                 }
-                //             }
-                //         }
-                //     }
-                // },
                 legend: {
-                    align: 'right',
-                    symbolRadius: 0,
-                    verticalAlign: 'top',
-                    x: 10,
-                    itemMarginTop: -10
+                    enabled: false
                 },
                 xAxis: {
                     labels: {
@@ -97,15 +86,19 @@ document.addEventListener('DOMContentLoaded', function () {
                     title: false,
                     labels: {
                         useHTML: true,
-                        overflow: 'allow'
-                    }
+                        overflow: 'allow',
+                        format: '{value:.1f}'
+                    },
+                    min: 0,
+                    max: 2.5
                 },
                 credits: {
                     enabled: false
                 },
                 tooltip: {
                     shadow: false,
-                    padding: 10
+                    padding: 10,
+                    pointFormat: '{point.low}% - {point.high}%</b>'
                 },
                 responsive: {
                     rules: [{
